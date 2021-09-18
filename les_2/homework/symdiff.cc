650b16d138ca8cdf2666548795dc21be96e80d6e
derzhavin3016 <derzhavin.andrej@yandex.ru>
2021-09-19 01:49:38 +0300

    [hw_2] Added symdiff task solution

---
+++ les_2/homework/symdiff.cc
@@ -0,0 +1,91 @@
+#include <iostream>
+#include <vector>
+#include <cmath>
+
+class set
+{
+private:
+  using T = uint64_t;
+  enum { CH_SIZE = 64 };
+
+  std::vector<T> chunks;
+public:
+  set( size_t size ) : chunks(size)
+  {
+  }
+
+  void insert( size_t num )
+  {
+    size_t ch_num = num / CH_SIZE,
+           ch_pos = num % CH_SIZE;
+    
+    chunks[ch_num] |= 1u << ch_pos;
+  }
+
+  bool in( size_t num ) const
+  {
+    size_t ch_num = num / CH_SIZE,
+           ch_pos = num % CH_SIZE;
+
+    return (chunks[ch_num] >> ch_pos) & 1;
+  }
+
+  void symd( const set &that )
+  {
+    size_t min_sz = std::min(that.chunks.size(), chunks.size());
+    for (size_t i = 0; i < min_sz; ++i)
+      chunks[i] ^= that.chunks[i];
+  }
+
+  std::ostream &out( std::ostream &ost ) const
+  {
+    for (size_t i = 0, end = CH_SIZE * chunks.size(); i < end; ++i)
+      if (in(i))
+        ost << i << " ";
+    ost << std::endl;
+
+    return ost;
+  }
+
+  ~set()
+  {}
+};
+
+std::ostream &operator <<( std::ostream &ost, const set &s )
+{
+  return s.out(ost);
+}
+
+set operator ^( const set &lhs, const set &rhs )
+{
+  set tmp{lhs};
+  tmp.symd(rhs);
+
+  return tmp;
+}
+
+constexpr size_t set_sz = 313;
+
+void set_inp( std::istream &ist, set &s )
+{
+  while (1)
+  {
+    size_t num = 0;
+    ist >> num;
+    if (num == 0)
+      break;
+    s.insert(num);
+  }
+}
+
+int main()
+{
+  set A{set_sz}, B{set_sz};
+
+  set_inp(std::cin, A);
+  set_inp(std::cin, B);
+
+  std::cout << (A ^ B);
+
+  return 0;
+}
